322b479b3382f7cae8727e30313a3064d3849fee
Matt Hunzinger <matthunz2@gmail.com>
2019-07-29 19:47:35 -0400

    Create Frame with decode method

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,36 @@
+#[derive(Debug, PartialEq)]
+pub enum Status {
+    Complete(usize),
+    Partial,
+}
+
+pub struct Head {
+    pub finished: bool,
+    pub rsv: [bool; 3],
+}
+
+#[derive(Default)]
+pub struct Frame {
+    pub head: Option<Head>,
+}
+
+impl Frame {
+    pub const fn empty() -> Self {
+        Self { head: None }
+    }
+    pub fn decode(&mut self, buf: &[u8]) -> Status {
+        Status::Partial
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
     fn it_works() {
-        assert_eq!(2 + 2, 4);
+        const BYTES: &[u8] = &[];
+        let mut f = Frame::empty();
+        assert_eq!(Status::Partial, f.decode(BYTES));
     }
 }
