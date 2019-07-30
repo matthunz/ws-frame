06b4c51a004943fd3e8fa97ffa1ae958714636b8
Matt Hunzinger <matthunz2@gmail.com>
2019-07-29 20:06:19 -0400

    Decode finished and rsv bits

--- src/lib.rs
+++ src/lib.rs
@@ -19,6 +19,18 @@ impl Frame {
         Self { head: None }
     }
     pub fn decode(&mut self, buf: &[u8]) -> Status {
+        let mut bytes = buf.iter();
+        if let Some(byte) = bytes.next() {
+            let finished = byte >> 7 == 1u8;
+            let rsv_bits = byte >> 4 & 0x7u8;
+
+            let mut rsv = [false; 3];
+            for i in 0..3 {
+                rsv[2 - i] = rsv_bits >> i & 0x1u8 == 1u8;
+            }
+
+            self.head = Some(Head { finished, rsv })
+        }
         Status::Partial
     }
 }
@@ -29,8 +41,12 @@ mod tests {
 
     #[test]
     fn it_works() {
-        const BYTES: &[u8] = &[];
+        const BYTES: &[u8] = &[0b10100010];
         let mut f = Frame::empty();
         assert_eq!(Status::Partial, f.decode(BYTES));
+
+        let head = f.head.unwrap();
+        assert!(head.finished);
+        assert_eq!([false, true, false], head.rsv);
     }
 }
